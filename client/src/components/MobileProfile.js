fdc782adf5fa31ef7627cf609cc72bedf3d00fb1
Zhijian Wang <zhijianjerrywang@gmail.com>
2023-12-12 05:14:33 -0800

    fixed bug???

--- client/src/components/MobileProfile.js
+++ client/src/components/MobileProfile.js
@@ -8,9 +8,7 @@ import {
 } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react";
-atar from "./user/UserAvatar";
-
-import React, { useEffect, useState } from "react";
+import UserAvatar from "./user/UserAvatar";
 
 import HorizontalStack from "./util/HorizontalStack";
 
