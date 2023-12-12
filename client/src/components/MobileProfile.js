72ed258297ad058bc0b074eac7591da05ac90b85
Emmanuel Jerez <75187415+3jsimba@users.noreply.github.com>
2023-12-12 05:10:23 -0800

    clean

--- client/src/components/MobileProfile.js
+++ client/src/components/MobileProfile.js
@@ -7,10 +7,9 @@ import {
   Divider, Stack, Typography,
 } from "@mui/material";
 import { Box } from "@mui/system";
-import React, { useEffect, useState } from "react";
-atar from "./user/UserAvatar";
 
 import React, { useEffect, useState } from "react";
+import UserAvatar from "./user/UserAvatar";
 
 import HorizontalStack from "./util/HorizontalStack";
 
